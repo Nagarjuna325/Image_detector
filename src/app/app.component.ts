2e007641e0d4c24d4b908adf890c0ddade1670a8
Nagarjuna325 <nagarjunreddybolla@gmail.com>
2024-07-10 13:21:28 -0500

    Changes made to correct NgModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FormsModule],
+  imports: [RouterOutlet, NavbarComponent, FormsModule, CommonModule , BrowserModule , NgModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
